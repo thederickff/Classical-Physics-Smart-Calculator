b23a23ea3a6ce990dfc470e13b436f0a7745ba92
derickfelix <derickfelix4321@gmail.com>
2017-03-18 16:08:31 -0300

    layout changed.

--- src/com/temperatureconverter/view/Screen.java
+++ src/com/temperatureconverter/view/Screen.java
@@ -32,22 +32,19 @@ public class Screen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        title = new javax.swing.JLabel();
-        cmbUnit = new javax.swing.JComboBox<>();
+        paneMain = new javax.swing.JPanel();
         spnValue = new javax.swing.JSpinner();
-        lblResult = new javax.swing.JLabel();
+        cmbUnit = new javax.swing.JComboBox<>();
         txtResult = new javax.swing.JTextField();
+        title = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Temperature Converter");
         setResizable(false);
 
-        title.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
-        title.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        title.setText("Temperature Converter");
-
-        cmbUnit.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
-        cmbUnit.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Select the unit", "Fahreinheit to Celsius", "Fahreinheit to Kelvin", "Celsius to Fahreinheit", "Celsius to Kelvin", "Kelvin to Fahreinheit", "Kelvin to Celsius" }));
+        paneMain.setBackground(new java.awt.Color(153, 153, 153));
+        paneMain.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        paneMain.setForeground(new java.awt.Color(255, 255, 51));
 
         spnValue.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
         spnValue.setModel(new javax.swing.SpinnerNumberModel());
@@ -57,53 +54,63 @@ public class Screen extends javax.swing.JFrame {
             }
         });
 
-        lblResult.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
-        lblResult.setText("Result");
+        cmbUnit.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
+        cmbUnit.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Select the unit", "Fahreinheit to Celsius", "Fahreinheit to Kelvin", "Celsius to Fahreinheit", "Celsius to Kelvin", "Kelvin to Fahreinheit", "Kelvin to Celsius" }));
 
         txtResult.setEditable(false);
         txtResult.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         txtResult.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         txtResult.setToolTipText("Result");
 
+        title.setFont(new java.awt.Font("Dialog", 1, 36)); // NOI18N
+        title.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        title.setText("Temperature Converter");
+
+        javax.swing.GroupLayout paneMainLayout = new javax.swing.GroupLayout(paneMain);
+        paneMain.setLayout(paneMainLayout);
+        paneMainLayout.setHorizontalGroup(
+            paneMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(paneMainLayout.createSequentialGroup()
+                .addGap(78, 78, 78)
+                .addComponent(spnValue, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(cmbUnit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtResult, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(136, Short.MAX_VALUE))
+            .addGroup(paneMainLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(title, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        paneMainLayout.setVerticalGroup(
+            paneMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, paneMainLayout.createSequentialGroup()
+                .addGap(53, 53, 53)
+                .addComponent(title)
+                .addGap(114, 114, 114)
+                .addGroup(paneMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cmbUnit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(spnValue, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtResult, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(141, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(172, 172, 172)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(67, 67, 67)
-                                .addComponent(lblResult))
-                            .addComponent(txtResult, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(34, 34, 34)
-                        .addComponent(title, javax.swing.GroupLayout.PREFERRED_SIZE, 482, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(352, 352, 352)
-                        .addComponent(spnValue, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(cmbUnit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(204, 204, 204)))
-                .addContainerGap(38, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(paneMain, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addComponent(title)
-                .addGap(32, 32, 32)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(spnValue, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(cmbUnit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(40, 40, 40)
-                .addComponent(lblResult)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(txtResult, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(78, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(paneMain, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -152,7 +159,7 @@ public class Screen extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> cmbUnit;
-    private javax.swing.JLabel lblResult;
+    private javax.swing.JPanel paneMain;
     private javax.swing.JSpinner spnValue;
     private javax.swing.JLabel title;
     private javax.swing.JTextField txtResult;
