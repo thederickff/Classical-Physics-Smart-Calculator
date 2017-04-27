f8b1517d78ba041b4e8782deeda61030301fa5e7
derickfelix <derickfelix4321@gmail.com>
2017-04-27 19:41:59 -0400

    Converter.java modified.

--- src/com/temperatureconverter/controller/Converter.java
+++ src/com/temperatureconverter/controller/Converter.java
@@ -6,9 +6,6 @@
 package com.temperatureconverter.controller;
 
 import com.temperatureconverter.model.Scale;
-import javax.swing.JComboBox;
-import javax.swing.JSpinner;
-import javax.swing.JTextField;
 
 /**
  *
@@ -23,35 +20,5 @@ public class Converter {
     public Converter() {
 
     }
-
-    public void stateChanged(JTextField txt, JSpinner spn, JComboBox cmb) {
-        int from = cmb.getSelectedIndex();
-        double value = (double) spn.getValue();
-        double result = 0;
-        switch (from) {
-            case 1:
-                result = (5 * (value - 32)) / 9;
-                break;
-            case 2:
-                result = ((5 * (value - 32)) / 9)+247;
-                break;
-            case 3:
-                result = ((value * 9) / 5) + 32;
-                break;
-            case 4:
-                result = value + 247;
-                break;
-            case 5:
-                result = (((value - 247) * 9) / 5) + 32;
-                break;
-            case 6:
-                result = value - 247;
-                break;
-            default:
-                System.out.println("Erro selecione um tipo!");
-        }
-         
-         txt.setText(Double.toString(result));
-
-    }
+    
 }
